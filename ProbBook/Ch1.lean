e9815cf991c65eee0b95939131f3c3317bc9423f
akp2003 <akp9132@gmail.com>
2023-11-22 04:52:32 +0330

    Prove Theorem 1.4

--- ProbBook/Ch1.lean
+++ ProbBook/Ch1.lean
@@ -40,6 +40,85 @@ instance : Coe (DiscreteDistFunc Ω) (DistFunc Ω) where
 
 def P [d : DistFunc Ω] (E : Set Ω) : ℝ := d.prob E
 
+-- toOuterMeasure and toPMF
+open MeasureTheory
+open MeasureTheory.OuterMeasure
+
+noncomputable
+def DiscreteDistFunc.toOuterMeasure (d : DiscreteDistFunc Ω) : OuterMeasure Ω :=
+  OuterMeasure.sum fun x : Ω => Real.toNNReal (d.m x) • dirac x
+
+lemma Summable.of_tsum_ne_zero {ι α : Type*} [AddCommMonoid α] [TopologicalSpace α] [T2Space α] {f : ι → α}
+   (hf : ∑' i, f i ≠ 0) : Summable f := not_imp_comm.1 tsum_eq_zero_of_not_summable hf
+
+
+lemma DiscreteDistFunc.summable [d : DiscreteDistFunc Ω] : Summable d.m := by
+    exact Summable.of_tsum_ne_zero (by simp [d.hu])
+
+--lemma fun_lem {A : Set Ω} {f : A → ℝ} {g : Ω → ℝ} (hg : g = fun i => if i∈A then f i else 0) (x : A) : f x = g x := by
+
+lemma DiscreteDistFunc.summable_set [d : DiscreteDistFunc Ω] (A : Set Ω) : Summable fun i : A => d.m i := by
+    let f := fun i : Ω => d.m i
+    set g := fun i : A => d.m i
+    have hs : Summable f := by
+      unfold_let
+      exact Summable.of_tsum_ne_zero (by simp [d.hu])
+    exact (summable_subtype_and_compl.mpr hs).left
+    --I can't believe I proved this theorem!
+    --Now I don't need to add Summable assumptions to my theorems
+    --exact? keeps searching but can't find this!
+
+-- Thanks to Yaël Dillies for proving this nice lemma
+lemma HasSum.of_tsum_ne_zero {ι α : Type*} [AddCommMonoid α] [TopologicalSpace α] [T2Space α] {f : ι → α}
+   (hf : ∑' i, f i ≠ 0) : HasSum f (∑' i, f i) := by
+  obtain ⟨x, hx⟩ : Summable f := Summable.of_tsum_ne_zero hf
+  rwa [hx.tsum_eq]
+
+theorem tsum_ofReal_eq {f : α → ℝ} (hs : Summable f) (hp : ∀a, f a ≥ 0):
+    ENNReal.ofReal (∑' a, f a) = ∑' a, ENNReal.ofReal (f a) := by
+  let g : α → NNReal := fun a => (f a).toNNReal
+  have hsg : Summable g := by exact Summable.toNNReal hs
+  have h1 : ENNReal.ofReal (∑' (a : α), f a) ≠ ⊤ := by exact ENNReal.ofReal_ne_top
+  have h2 : ∑' (a : α), ENNReal.ofReal (f a) ≠ ⊤ := by exact ENNReal.tsum_coe_ne_top_iff_summable.mpr hsg
+  rw [← ENNReal.toReal_eq_toReal h1 h2]
+  rw [ENNReal.tsum_toReal_eq]
+  rw [ENNReal.toReal_ofReal]
+  have he (a : α) : f a = (fun a => (ENNReal.ofReal (f a)).toReal) a := by
+    simp
+    rw [ENNReal.toReal_ofReal ]
+    exact hp a
+  rw [tsum_congr he]
+  exact tsum_nonneg hp
+  exact fun a ↦ ENNReal.ofReal_ne_top
+
+noncomputable
+def DiscreteDistFunc.toPMF (d : DiscreteDistFunc Ω) : PMF Ω := {
+  val := fun ω : Ω => ENNReal.ofReal (d.m ω)
+  property := by
+    let f : Ω → ENNReal := fun ω : Ω => ENNReal.ofReal (d.m ω)
+    have hz : ∑' (ω : Ω), d.m ω ≠ 0 := by
+      simp [d.hu]
+    have hnt (ω : Ω) : ENNReal.ofReal (d.m ω) ≠ ⊤ := by
+      aesop
+    have h3 : ∑' (ω : Ω), f ω = ENNReal.ofReal (∑' (ω : Ω), d.m ω) := by
+      unfold_let
+      simp
+      rw [tsum_ofReal_eq (Summable.of_tsum_ne_zero hz)]
+      exact hp
+    have hs : ∑' (ω : Ω), f ω = 1 := by
+      simp_all only [ne_eq, f]
+      simp [ENNReal.tsum_toReal_eq hnt]
+      exact hu
+    have hz2 : ∑' (ω : Ω), f ω ≠ 0 := by
+      simp [hs]
+    rw [← hs]
+    simp [HasSum.of_tsum_ne_zero hz2]
+}
+
+noncomputable
+def DiscreteDistFunc.toMeasure (d : DiscreteDistFunc Ω) [MeasurableSpace Ω] : Measure Ω :=
+  d.toPMF.toMeasure
+
 -- Example 1.7
 namespace Example_1_7
 
@@ -237,7 +316,7 @@ theorem P_inter_add_compl [d : DiscreteDistFunc Ω]
     | 1 => Bᶜ
     | _ => ∅
   have hpd : (Finset.range 2 : Set ℕ).Pairwise (Disjoint on B') := by
-    intro x y hxy
+    intro x xin y yin xy
     rw [Function.onFun_apply]
     have h1 : Disjoint B Bᶜ := Set.disjoint_compl_right_iff_subset.mpr fun ⦃a⦄ a ↦ a
     have h2 : Disjoint Bᶜ B := id (Disjoint.symm h1)
@@ -260,68 +339,50 @@ theorem P_inter_add_compl [d : DiscreteDistFunc Ω]
   rw [sum_range_succ]
   simp
 
--- toOuterMeasure and toPMF
-open MeasureTheory
-open MeasureTheory.OuterMeasure
-
-noncomputable
-def DiscreteDistFunc.toOuterMeasure (d : DiscreteDistFunc Ω) : OuterMeasure Ω :=
-  OuterMeasure.sum fun x : Ω => Real.toNNReal (d.m x) • dirac x
-
-lemma Summable.of_tsum_ne_zero {ι α : Type*} [AddCommMonoid α] [TopologicalSpace α] [T2Space α] {f : ι → α}
-   (hf : ∑' i, f i ≠ 0) : Summable f := not_imp_comm.1 tsum_eq_zero_of_not_summable hf
-
--- Thanks to Yaël Dillies for proving this nice lemma
-lemma HasSum.of_tsum_ne_zero {ι α : Type*} [AddCommMonoid α] [TopologicalSpace α] [T2Space α] {f : ι → α}
-   (hf : ∑' i, f i ≠ 0) : HasSum f (∑' i, f i) := by
-  obtain ⟨x, hx⟩ : Summable f := Summable.of_tsum_ne_zero hf
-  rwa [hx.tsum_eq]
-
-theorem tsum_ofReal_eq {f : α → ℝ} (hs : Summable f) (hp : ∀a, f a ≥ 0):
-    ENNReal.ofReal (∑' a, f a) = ∑' a, ENNReal.ofReal (f a) := by
-  let g : α → NNReal := fun a => (f a).toNNReal
-  have hsg : Summable g := by exact Summable.toNNReal hs
-  have h1 : ENNReal.ofReal (∑' (a : α), f a) ≠ ⊤ := by exact ENNReal.ofReal_ne_top
-  have h2 : ∑' (a : α), ENNReal.ofReal (f a) ≠ ⊤ := by exact ENNReal.tsum_coe_ne_top_iff_summable.mpr hsg
-  rw [← ENNReal.toReal_eq_toReal h1 h2]
-  rw [ENNReal.tsum_toReal_eq]
-  rw [ENNReal.toReal_ofReal]
-  have he (a : α) : f a = (fun a => (ENNReal.ofReal (f a)).toReal) a := by
-    simp
-    rw [ENNReal.toReal_ofReal ]
-    exact hp a
-  rw [tsum_congr he]
-  exact tsum_nonneg hp
-  exact fun a ↦ ENNReal.ofReal_ne_top
-
-noncomputable
-def DiscreteDistFunc.toPMF (d : DiscreteDistFunc Ω) : PMF Ω := {
-  val := fun ω : Ω => ENNReal.ofReal (d.m ω)
-  property := by
-    let f : Ω → ENNReal := fun ω : Ω => ENNReal.ofReal (d.m ω)
-    have hz : ∑' (ω : Ω), d.m ω ≠ 0 := by
-      simp [d.hu]
-    have hnt (ω : Ω) : ENNReal.ofReal (d.m ω) ≠ ⊤ := by
-      aesop
-    have h3 : ∑' (ω : Ω), f ω = ENNReal.ofReal (∑' (ω : Ω), d.m ω) := by
-      unfold_let
-      simp
-      rw [tsum_ofReal_eq (Summable.of_tsum_ne_zero hz)]
-      exact hp
-    have hs : ∑' (ω : Ω), f ω = 1 := by
-      simp_all only [ne_eq, f]
-      simp [ENNReal.tsum_toReal_eq hnt]
-      exact hu
-    have hz2 : ∑' (ω : Ω), f ω ≠ 0 := by
-      simp [hs]
-    rw [← hs]
-    simp [HasSum.of_tsum_ne_zero hz2]
-}
-
-
+-- Need this for theorem 1.4
+theorem P_sdiff [d : DiscreteDistFunc Ω]
+  (A : Set Ω) (B : Set Ω):
+    P (A \ B) = P A - P (A ∩ B) := by
+  have hd : Disjoint (A \ B) (A ∩ B) := by exact Set.disjoint_sdiff_inter
+  have hu : A = (A \ B) ∪ (A ∩ B) := by exact Eq.symm (Set.diff_union_inter A B)
+  rw [@eq_sub_iff_add_eq]
+  rw [← P_union_disjoint (A \ B) (A ∩ B) hd (d.summable_set (A \ B)) (d.summable_set (A ∩ B))]
+  rw [← hu]
 
 --Theorem 1.4
 theorem P_union [d : DiscreteDistFunc Ω]
   (A : Set Ω) (B : Set Ω):
     P (A ∪ B) = P A + P B - P (A ∩ B) := by
-  sorry
+  let C (i : ℕ) : Set Ω :=
+    match i with
+    | 0 => A \ B
+    | 1 => A ∩ B
+    | 2 => B \ A
+    | _ => ∅
+  have hpd : (Finset.range 3 : Set ℕ).Pairwise (Disjoint on C) := by
+    intro x xin y yin xy
+    rw [Function.onFun_apply]
+    simp [C]
+    have hd1 : Disjoint (A \ B) (A ∩ B) := by exact Set.disjoint_sdiff_inter
+    have hd2 : Disjoint (A \ B) (B \ A) := by exact disjoint_sdiff_sdiff
+    have hd3 : Disjoint (B \ A) (A ∩ B) := by rw [Set.inter_comm] ; exact Set.disjoint_sdiff_inter
+    have hd4 : Disjoint (A ∩ B) (A \ B) := by rw [disjoint_comm] ; exact hd1
+    have hd5 : Disjoint (A ∩ B) (B \ A) := by exact id (Disjoint.symm hd3)
+    have hd6 : Disjoint (B \ A) (A \ B) := by exact id (Disjoint.symm hd2)
+    aesop
+  have hsum : ∀ (j : ℕ), Summable fun i : C j ↦ d.m ↑i := by exact fun j ↦ DiscreteDistFunc.summable_set (C j)
+  have hu : A ∪ B = ⋃ i ∈ range 3, C i := by
+    rw [range_succ]
+    rw [range_succ]
+    aesop
+  rw [hu]
+  rw [P_fin_pairwise_disjoint_union 3 C hpd hsum]
+  rw [sum_range_succ]
+  rw [sum_range_succ]
+  rw [sum_range_succ]
+  unfold_let
+  simp
+  rw [P_sdiff A B]
+  rw [P_sdiff B A]
+  rw [Set.inter_comm]
+  ring
