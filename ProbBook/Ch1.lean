e9980ec82be12b94c38fedb93367bffb9e2fdc73
akp2003 <akp9132@gmail.com>
2024-07-20 20:29:27 +0330

    Add theorem 1.1 and example 1.7 but they are incomplete

--- ProbBook/Ch1.lean
+++ ProbBook/Ch1.lean
@@ -1,11 +1,64 @@
 import Mathlib.Init.Set
 import Mathlib.Topology.Instances.ENNReal
+import Mathlib
+
+variable {Ω : Type u}
 
 -- Definition 1.2
 structure DistFunc (Ω : Type u) where
   m : Ω → ℝ
-  hp : m ω ≥ 0
+  hp (ω : Ω) : m ω ≥ 0
   hs : (∑' ω, (m ω)) = 1
 
 noncomputable
-def Prob {Ω : Type u} (df : DistFunc Ω) (E : Set Ω) := (∑' (ω : E), (df.m ω))
+def P (dist : DistFunc Ω) (E : Set Ω) := (∑' (ω : E), (dist.m ω))
+
+-- Example 1.7
+namespace Example_1_7
+
+inductive Coin2 where
+  | HH
+  | HT
+  | TH
+  | TT
+  deriving Fintype
+
+open Coin2
+
+noncomputable
+def dist : DistFunc Coin2 := {
+  m := fun (x : Coin2) => 1/4
+  hp := by simp
+  hs := by
+    rw [one_div, tsum_const]
+    have hc : Nat.card Coin2 = 4 := by aesop
+    rw [hc]
+    norm_num
+}
+
+lemma dist_pairwise : Pairwise (fun (x y : Coin2) => dist.m x = dist.m y) := by
+  exact fun ⦃i j⦄ ↦ congrFun rfl
+
+def E : Set Coin2 := {HH,HT,TH}
+
+example : P dist E = 3/4 := by
+  rw [P, dist]
+  have hc : Nat.card E = 3 := by
+    rw [E]
+    sorry
+  simp
+  rw [hc]
+  norm_num
+
+end Example_1_7
+
+--Theorem 1.1
+--1
+theorem P_positivity (dist : DistFunc Ω) (E : Set Ω) (hsum : Summable fun i : E ↦ dist.m ↑i):
+  P dist E ≥ 0 := by
+  rw [P]
+  have hp (ω : E) : dist.m ω ≥ 0 := by
+    exact dist.hp ↑ω
+  calc
+    ∑' (ω : ↑E), dist.m ↑ω ≥ ∑' (i : ↑E), 0 := by rel [(tsum_le_tsum hp (summable_zero) (hsum))]
+    _ ≥ 0 := by simp
