d5e6b728c756ece07e69c38a60abba49bd298a73
akp2003 <akp9132@gmail.com>
2024-08-02 17:22:56 +0330

    Add Corollary 1.1

--- ProbBook/Ch1.lean
+++ ProbBook/Ch1.lean
@@ -112,22 +112,16 @@ lemma P_empty [DistFunc Ω] : P (∅ : Set Ω) = 0 := by
   rw [P]
   exact tsum_empty
 
+open Finset
 --Theorem 1.2
 theorem P_fin_pairwise_disjoint_union [d : DistFunc Ω]
   (n : ℕ)
-  (A : (Fin n) → Set Ω)
-  (hpd : Pairwise (Disjoint on A))
+  (A : ℕ → Set Ω)
+  (hpd : (range n : Set ℕ).Pairwise (Disjoint on A))
   (hsum : ∀j , Summable fun i : A j ↦ d.m ↑i):
-    P (⋃ i , A i) = ∑' i , P (A i) := by
-  have hpd2 : ((Finset.univ : Finset (Fin n)) : Set (Fin n)).Pairwise (Disjoint on A) := by
-    rw [Pairwise] at hpd
-    intro x _ y _
-    exact fun a ↦ hpd a
-  have hu : (⋃ i , A i) = ⋃ i ∈ Finset.univ, A i := by simp
+    P (⋃ i ∈ range n , A i) = ∑ i ∈ (range n), P (A i) := by
   unfold P
-  rw [hu]
-  rw [tsum_finset_bUnion_disjoint hpd2 (by exact fun i _ ↦ hsum i)]
-  exact Eq.symm (tsum_fintype fun b ↦ ∑' (ω : ↑(A b)), d.m ↑ω)
+  rw [tsum_finset_bUnion_disjoint hpd (by exact fun i _ ↦ hsum i)]
 
 --Some modified versions
 theorem P_fin_pairwise_disjoint_union2 [d : DistFunc Ω]
@@ -156,28 +150,82 @@ theorem P_fin_pairwise_disjoint_union3 [d : DistFunc Ω]
   rw [tsum_finset_bUnion_disjoint hpd (by exact fun i _ ↦ hsum i)]
   rw [←(Finset.tsum_subtype I (fun i => (∑' (x : ↑(A i)), d.m ↑x)))]
 
+theorem P_fin_pairwise_disjoint_union4 [d : DistFunc Ω]
+  (n : ℕ)
+  (A : (Fin n) → Set Ω)
+  (hpd : Pairwise (Disjoint on A))
+  (hsum : ∀j , Summable fun i : A j ↦ d.m ↑i):
+    P (⋃ i , A i) = ∑' i , P (A i) := by
+  have hpd2 : ((Finset.univ : Finset (Fin n)) : Set (Fin n)).Pairwise (Disjoint on A) := by
+    rw [Pairwise] at hpd
+    intro x _ y _
+    exact fun a ↦ hpd a
+  have hu : (⋃ i , A i) = ⋃ i ∈ Finset.univ, A i := by simp
+  unfold P
+  rw [hu]
+  rw [tsum_finset_bUnion_disjoint hpd2 (by exact fun i _ ↦ hsum i)]
+  exact Eq.symm (tsum_fintype fun b ↦ ∑' (ω : ↑(A b)), d.m ↑ω)
+
 --Theorem 1.3
 theorem P_fin_pairwise_disjoint_inter [d : DistFunc Ω]
-  (n : ℕ) (A : (Fin n) → Set Ω)
-  (hpd : Pairwise (Disjoint on A))
-  (huniv : Set.univ = (⋃ i, A i)) (E : Set Ω)
+  (n : ℕ) (A : ℕ → Set Ω)
+  (hpd : (Finset.range n : Set ℕ).Pairwise (Disjoint on A))
+  (huniv : Set.univ = (⋃ i ∈ range n, A i)) (E : Set Ω)
   (hsumEA : ∀j , Summable fun i : ↑(E ∩ A j) ↦ d.m ↑i):
-    P E = ∑' i, P (E ∩ A i) := by
-  let B : (Fin n) → Set Ω := fun i => E ∩ A i
-  have hpdB : Pairwise (Disjoint on B) := by
+    P E = ∑ i ∈ range n, P (E ∩ A i) := by
+  let B : ℕ → Set Ω := fun i => E ∩ A i
+  have hpdB : (Finset.range n : Set ℕ).Pairwise (Disjoint on B) := by
     unfold_let
-    intro j k hjk
+    intro x xin y yin xy
     rw [Function.onFun_apply]
     rw [Set.disjoint_iff_inter_eq_empty]
     rw [← Set.inter_inter_distrib_left]
-    replace hpd : (Disjoint on A) j k := by exact hpd hjk
+    replace hpd : (Disjoint on A) x y := by exact hpd xin yin xy
     rw [Function.onFun_apply] at hpd
     rw [Set.disjoint_iff_inter_eq_empty] at hpd
     simp_all only [Set.disjoint_iUnion_left, ne_eq, Set.inter_empty]
   have hsumB : ∀j , Summable fun i : B j ↦ d.m ↑i := by exact fun j ↦ hsumEA j
-  have hu : ⋃ i, B i = E := by
+  have hu : ⋃ i ∈ range n, B i = E := by
     unfold_let
-    rw [← Set.inter_iUnion]
-    simp [← huniv]
+    simp only
+    calc
+    ⋃ i ∈ range n, E ∩ A i = E ∩ (⋃ i ∈ range n, A i) := by exact Eq.symm (Set.inter_iUnion₂ E fun i _ ↦ A i)
+    _ = E ∩ Set.univ := by exact congrArg (Inter.inter E) (id (Eq.symm huniv))
+    _ = E := by simp
   rw [← P_fin_pairwise_disjoint_union n B hpdB hsumB]
   rw [hu]
+
+--Corollary 1.1
+theorem P_inter_add_compl [d : DistFunc Ω]
+  (A : Set Ω) (B : Set Ω)
+  (hsum : Summable fun i : (Set.inter A B) ↦ d.m ↑i)
+  (hsumc : Summable fun i : (Set.inter A Bᶜ) ↦ d.m ↑i):
+    P A = P (A ∩ B) + P (A ∩ Bᶜ) := by
+  let B' (i : ℕ) : Set Ω :=
+    match i with
+    | 0 => B
+    | 1 => Bᶜ
+    | _ => ∅
+  have hpd : (Finset.range 2 : Set ℕ).Pairwise (Disjoint on B') := by
+    intro x y hxy
+    rw [Function.onFun_apply]
+    have h1 : Disjoint B Bᶜ := Set.disjoint_compl_right_iff_subset.mpr fun ⦃a⦄ a ↦ a
+    have h2 : Disjoint Bᶜ B := id (Disjoint.symm h1)
+    aesop
+  have huniv : Set.univ = ⋃ i ∈ range 2, B' i := by
+    rw [range_succ]
+    simp_all only [coe_range, range_one, mem_insert, mem_singleton, Set.iUnion_iUnion_eq_or_left,Set.iUnion_iUnion_eq_left, Set.compl_union_self, B']
+  have hsumB' : ∀j, Summable fun (i : @Inter.inter (Set Ω) _ A (B' j)) ↦ d.m ↑i := by
+    intro j
+    match j with
+      | 0 => exact hsum
+      | 1 => exact hsumc
+      | x+2 =>
+        have hx2 : B' (x+2) = ∅ := rfl
+        rw [hx2]
+        rw [Set.inter_empty A]
+        simp [summable_empty]
+  rw [P_fin_pairwise_disjoint_inter 2 B' hpd huniv A hsumB']
+  rw [sum_range_succ]
+  rw [sum_range_succ]
+  simp
