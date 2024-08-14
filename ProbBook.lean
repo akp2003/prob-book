e5b09f6fecec9e6bf3aae6ed860605bae076cd44
akp2003 <akp9132@gmail.com>
2024-08-15 02:25:50 +0330

    Change Ch1-Examples to Ch1_Examples

--- ProbBook.lean
+++ ProbBook.lean
@@ -2,5 +2,5 @@
 -- Import modules here that should be built as part of the library.
 import «ProbBook».Basic
 import «ProbBook».Ch1
-import «ProbBook».«Ch1-Examples»
+import «ProbBook».Ch1_Examples
 import «ProbBook».Ch2
